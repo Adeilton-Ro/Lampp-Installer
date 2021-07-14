1856eec6b31bb18e93ca9311fd8d1d131e5cf11c
Adeilton-Ro <adeiltonjr15@gmail.com>
2021-07-14 12:59:51 -0300

    edit Remove file

--- src/scripts/php/Remove.php
+++ src/scripts/php/Remove.php
@@ -2,5 +2,5 @@
     $path = $_POST["path"];
     $directory = $_POST["directory"];
     $Extension = $_POST["extension"];
-    echo shell_exec("rm -rf $path/$directory.$Extension");
+    unlink("$path/$directory.$Extension");
 ?>
